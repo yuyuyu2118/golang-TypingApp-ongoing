ad82cb3cfc8604d3be5aa75a6430808090d754ca
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-05-19 00:15:14 +0900

    WeaponShopの追加と画像の追加

---
+++ shops.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/colornames"
+)
+
+var (
+	woodenKatanaButton = pixel.Rect{}
+	ironKatanaButton   = pixel.Rect{}
+	goldKatanaButton   = pixel.Rect{}
+	buttonSlice        = []pixel.Rect{}
+)
+
+func initWeapon(win *pixelgl.Window, Txt *text.Text, windowHeightSize int) {
+	//初期位置
+	yOffSet := win.Bounds().H() / 4
+	txtPos := pixel.V(0, 0)
+	Txt.Color = colornames.White
+	//csv読み込み
+	weaponSlice := []string{"1. WoodenKatana", "2. IronKatana", "3. goldKatanaButton", "4", "5", "6", "7", "8", "9", "10"}
+	descWeapon1 := []string{"WoodenKatana", "攻撃力3", "この剣はサンプルの剣です\n説明はサンプルです。", "強化回数", "未強化", "強化上昇値", "特殊能力", "この剣の特殊能力は？です。\nサンプルです。"}
+
+	for _, weaponName := range weaponSlice {
+		Txt.Clear()
+		fmt.Fprintln(Txt, weaponName)
+		yOffSet -= Txt.LineHeight + 20
+		txtPos = pixel.V(0, yOffSet)
+		tempPosition := pixel.IM.Moved(txtPos)
+		Txt.Draw(win, tempPosition)
+		buttonSlice = append(buttonSlice, Txt.Bounds().Moved(txtPos))
+	}
+
+	if win.JustPressed(pixelgl.Key1) {
+		Txt.Clear()
+		fmt.Fprintln(Txt, weaponSlice[0])
+
+	}
+}
+
+func weaponClickEvent(win *pixelgl.Window, mousePos pixel.Vec, currentGameState GameState) GameState {
+	//TODO ページを作成したら追加
+	if buttonSlice[0].Contains(mousePos) || win.JustPressed(pixelgl.Key1) {
+		currentGameState = WeaponShop
+		log.Println("Town->WeaponShop")
+	} else if buttonSlice[1].Contains(mousePos) || win.JustPressed(pixelgl.Key2) {
+		currentGameState = ArmorShop
+		log.Println("Town->ArmorShop")
+	} else if buttonSlice[2].Contains(mousePos) || win.JustPressed(pixelgl.Key3) {
+		currentGameState = AccessoryShop
+		log.Println("Town->AccessoryShop")
+	}
+	return currentGameState
+}
