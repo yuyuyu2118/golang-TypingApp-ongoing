942ff5d0b1844091e80627092195c7273397f220
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-02 17:50:07 +0900

    バグ修正

--- battle/battle.go
+++ battle/battle.go
@@ -89,6 +89,11 @@ func BattleTypingTest(win *pixelgl.Window, player *player.PlayerStatus, elapsed 
 	if myState.CurrentGS == myState.PlayingScreen {
 		if tempCount > 0 {
 			if typed != "" {
+				// if (temp[index] == 'j' && index < len(question)) && typed[0] == 'z' {
+				// 	log.Println("koko", temp[index])
+				// 	question = strings.NewReplacer("j", "").Replace(words[wordsNum])
+				// 	temp = []byte(question)
+				// }
 				if typed[0] == temp[index] && index < len(question) {
 					index++
 					collectType++
