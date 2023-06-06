c9826eae9b6e833e55135462b34c7bf25eece1b0
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-06 18:27:31 +0900

    武器全種のスキル追加 防具9種のスキル追加

--- battle/UniqueSkills.go
+++ battle/UniqueSkills.go
@@ -13,37 +13,40 @@ import (
 )
 
 var weaponName = []string{"木の棒", "果物ナイフ", "木刀", "ドレインソード", "スタンハンマー", "鉄の剣", "隼の剣", "勇者の剣", "名刀村正", "死神の大鎌"}
+var armorName = []string{"草織りのローブ", "フルーツアーマー", "木の鎧", "ソウルバインドプレート", "スタンプレート", "鉄の鎧", "飛翔のマント", "勇者の鎧", "刃舞の衣", "冥界の鎧"}
+var accessoryName = []string{"樹木のペンダント", "フルーツブレスレット", "平和のバンド", "ライフリンクのリング", "ショックウェーブリング", "鉄のブレスレット", "疾走のリング", "勇者のペンダント", "刀匠の指輪", "霊魂のイヤリング"}
 
-func AttackRelationSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus, tempWordDamage *float64) {
+func AttackRelationWeaponSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus, tempWordDamage *float64) {
 	if player.EquipmentWeapon[0] == weaponName[5] {
 		if rand.Float64() <= 0.1 {
-			UniqueSkill(win, (*tempWordDamage)*1.5, win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Aqua, player)
+			UniqueSkill(win, (*tempWordDamage)*1.5, win.Bounds().Center().Sub(pixel.V(0, 150)), colornames.Aqua, player, "")
 			enemy.HP += (*tempWordDamage) * 1.5
 		}
 	} else if player.EquipmentWeapon[0] == weaponName[6] {
 		for i := 0; i < rand.Intn(3); i++ {
-			UniqueSkill(win, (*tempWordDamage)*0.5, win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Yellow, player)
+			randomPotision := 50.0 * float64(i)
+			UniqueSkill(win, (*tempWordDamage)*0.5, win.Bounds().Center().Sub(pixel.V(0, 150+randomPotision)), colornames.Yellow, player, "")
 			enemy.HP += (*tempWordDamage) * 0.5
 		}
 	} else if player.EquipmentWeapon[0] == weaponName[7] {
 		log.Println(myGame.StageNum)
 		if rand.Float64() <= 0.2 {
-			UniqueSkill(win, (*tempWordDamage)*(float64(myGame.StageNum)*0.25), win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Orange, player)
+			UniqueSkill(win, (*tempWordDamage)*(float64(myGame.StageNum)*0.25), win.Bounds().Center().Sub(pixel.V(0, 150)), colornames.Orange, player, "")
 			enemy.HP += (*tempWordDamage) * (float64(myGame.StageNum) * 0.25)
 		}
 	} else if player.EquipmentWeapon[0] == weaponName[8] {
 		log.Println(myGame.StageNum)
 		if rand.Float64() <= 0.3 {
-			UniqueSkill(win, (*tempWordDamage)*1.5, win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Purple, player)
+			UniqueSkill(win, (*tempWordDamage)*1.5, win.Bounds().Center().Sub(pixel.V(0, 150)), colornames.Purple, player, "")
 			enemy.HP += (*tempWordDamage) * 1.5
 		}
 	}
 }
 
-func RecoveryRelationSkill(win *pixelgl.Window, player *player.PlayerStatus, tempWordDamage *float64) {
+func RecoveryRelationWeaponSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus, tempWordDamage *float64) {
 	if player.EquipmentWeapon[0] == weaponName[3] {
 		if rand.Float64() <= 0.1 {
-			UniqueSkill(win, -(*tempWordDamage)*0.01, win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Green, player)
+			UniqueSkill(win, -(*tempWordDamage)*0.01, win.Bounds().Center().Sub(pixel.V(0, 150)), colornames.Green, player, "")
 			player.HP += -(*tempWordDamage) * 0.01
 			if player.HP >= player.MaxHP {
 				player.HP = player.MaxHP
@@ -53,21 +56,87 @@ func RecoveryRelationSkill(win *pixelgl.Window, player *player.PlayerStatus, tem
 	//if player.EquipmentWeapon
 }
 
-func TimerRelationSkill(win *pixelgl.Window, player *player.PlayerStatus, tempTimer *float64) float64 {
+func TimerRelationWeaponSkill(win *pixelgl.Window, player *player.PlayerStatus, tempTimer *float64) float64 {
 	if player.EquipmentWeapon[0] == weaponName[4] {
 		if rand.Float64() <= 0.1 {
-			UniqueSkill(win, 1.0, win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Blue, player)
+			UniqueSkill(win, 1.0, win.Bounds().Center().Sub(pixel.V(0, 150)), colornames.Blue, player, "")
 			return 1.0
 		}
 	}
 	return 0.0
 }
 
-func DebuffRelationSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus) {
+func DebuffRelationWeaponSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus) {
 	if player.EquipmentWeapon[0] == weaponName[9] {
 		if rand.Float64() <= 0.15 {
-			UniqueSkill(win, -1.0, win.Bounds().Center().Sub(pixel.V(50, 250)), colornames.Red, player)
+			UniqueSkill(win, -1.0, win.Bounds().Center().Sub(pixel.V(0, 150)), colornames.Red, player, "")
 			enemy.OP -= 1.0
+		}
+	}
+}
+
+func AttackRelationArmorSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus, tempWordDamage *float64) {
+	if player.EquipmentArmor[0] == armorName[8] {
+		for i := 0; i < rand.Intn(3); i++ {
+			randomPotision := 50.0 * float64(i)
+			UniqueSkill(win, (*tempWordDamage)*0.5, win.Bounds().Center().Sub(pixel.V(0, 200+randomPotision)), colornames.Yellow, player, "ShadowSwift! ")
+			enemy.HP += (*tempWordDamage) * 0.5
+		}
+	}
+}
+
+func RecoveryRelationArmorSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus, tempWordDamage *float64) {
+	if player.EquipmentArmor[0] == armorName[3] {
+		if rand.Float64() <= 0.1 {
+			UniqueSkill(win, enemy.MaxOP*0.05, win.Bounds().Center().Sub(pixel.V(0, 200)), colornames.Whitesmoke, player, "")
+			player.HP += enemy.MaxOP * 0.05
+			if player.HP >= player.MaxHP {
+				player.HP = player.MaxHP
+			}
+		}
+	} else if player.EquipmentArmor[0] == armorName[7] {
+		if rand.Float64() <= 0.1 {
+			randomValue := 0.5 + (rand.Float64() * 1)
+			roundedRandomValue := float64(int(randomValue*10)) / 10
+			UniqueSkill(win, roundedRandomValue, win.Bounds().Center().Sub(pixel.V(0, 200)), colornames.Green, player, "BraveRecovery! ")
+			player.HP += roundedRandomValue
+			if player.HP >= player.MaxHP {
+				player.HP = player.MaxHP
+			}
+		}
+	}
+}
+
+func TimerRelationArmorSkill(win *pixelgl.Window, player *player.PlayerStatus, tempTimer *float64) float64 {
+	if player.EquipmentArmor[0] == armorName[4] {
+		if rand.Float64() <= 0.1 {
+			UniqueSkill(win, 1.0, win.Bounds().Center().Sub(pixel.V(0, 200)), colornames.Orange, player, "")
+			return 1.0
+		}
+	}
+	return 0.0
+}
+
+func DefenceRelationArmorSkill(win *pixelgl.Window, player *player.PlayerStatus, enemy *enemy.EnemyStatus) {
+	if player.EquipmentArmor[0] == armorName[5] {
+		if rand.Float64() <= 0.1 {
+			UniqueSkill(win, player.DP*1.5, win.Bounds().Center().Sub(pixel.V(0, 200)), colornames.Silver, player, "")
+			player.DP *= 1.5
+		}
+	} else if player.EquipmentArmor[0] == armorName[6] {
+		if rand.Float64() <= 1.0 {
+			UniqueSkill(win, -99, win.Bounds().Center().Sub(pixel.V(0, 200)), colornames.Silver, player, "")
+			enemy.OP -= 99
+		}
+	} else if player.EquipmentArmor[0] == armorName[7] {
+		if rand.Float64() <= 0.1 {
+			UniqueSkill(win, player.DP*1.25, win.Bounds().Center().Sub(pixel.V(0, 250)), colornames.Silver, player, "BraveDefence! ")
+			player.DP *= 1.25
+		}
+	} else if player.EquipmentArmor[0] == armorName[8] {
+		if rand.Float64() <= 1.0 {
+			UniqueSkill(win, -99, win.Bounds().Center().Sub(pixel.V(0, 200)), colornames.Silver, player, "ShadowEscape! ")
+			enemy.OP -= 99
 		}
 	}
 }
