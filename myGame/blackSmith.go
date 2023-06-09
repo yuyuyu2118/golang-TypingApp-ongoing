a5369e6e99a9688129415615bf799b733c02411f
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-09 11:55:19 +0900

    v1.0.3test

--- myGame/blackSmith.go
+++ myGame/blackSmith.go
@@ -49,11 +49,11 @@ func InitBlackSmith(win *pixelgl.Window, Txt *text.Text, player *myPlayer.Player
 		if myState.CurrentBlackSmith == myState.WeaponBlackSmith && myUtil.AnyKeyJustPressed(win, pg.Key1, pg.Key2, pg.Key3, pg.Key4, pg.Key5, pg.Key6, pg.Key7, pg.Key8, pg.Key9, pg.Key0, pg.KeyBackspace) {
 			WeaponBlackSmithClickEvent(win, win.MousePosition(), player)
 		}
-	case myState.ArmorBlackSmith:
-		InitArmorBlackSmithScreen(win, myUtil.DescriptionTxt, player)
-		if myState.CurrentBlackSmith == myState.ArmorBlackSmith && myUtil.AnyKeyJustPressed(win, pg.Key1, pg.Key2, pg.Key3, pg.Key4, pg.Key5, pg.Key6, pg.Key7, pg.Key8, pg.Key9, pg.Key0, pg.KeyBackspace) {
-			ArmorBlackSmithClickEvent(win, win.MousePosition(), player)
-		}
+		// case myState.ArmorBlackSmith:
+		// 	InitArmorBlackSmithScreen(win, myUtil.DescriptionTxt, player)
+		// 	if myState.CurrentBlackSmith == myState.ArmorBlackSmith && myUtil.AnyKeyJustPressed(win, pg.Key1, pg.Key2, pg.Key3, pg.Key4, pg.Key5, pg.Key6, pg.Key7, pg.Key8, pg.Key9, pg.Key0, pg.KeyBackspace) {
+		// 		ArmorBlackSmithClickEvent(win, win.MousePosition(), player)
+		// 	}
 		// case myState.AccessoryBlackSmith:
 		// 	InitAccessoryBlackSmithScreen(win, myUtil.DescriptionTxt, player)
 		// 	if myState.CurrentBlackSmith == myState.AccessoryBlackSmith && myUtil.AnyKeyJustPressed(win, pg.Key1, pg.Key2, pg.Key3, pg.Key4, pg.Key5, pg.Key6, pg.Key7, pg.Key8, pg.Key9, pg.Key0, pg.KeyBackspace) {
@@ -211,7 +211,7 @@ func BlackSmithClickEvent(win *pixelgl.Window, mousePos pixel.Vec, player *myPla
 				coefficient, _ := strconv.ParseFloat(loadContent[9][i], 64)
 				tempWeaponEnhancement, _ := strconv.ParseFloat(descWeapon[i+1][25], 64)
 				tempOP4 = tempWeaponEnhancement * coefficient
-				log.Println("player", coefficient, tempWeaponEnhancement)
+				//log.Println("player", coefficient, tempWeaponEnhancement)
 			}
 		}
 
